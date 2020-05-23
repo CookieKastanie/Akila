d2146e16f6a1bce8f25d26a6eeceb0e5dc854065
CookieKastanie <jerem0205@yahoo.fr>
2020-05-23 21:22:52 +0200

    ...

--- collision/SAT2d.js
+++ collision/SAT2d.js
@@ -10,63 +10,91 @@ const multMat3 = (out, a, m, z = 1) => {
     return out;
 }
 
+//////////////////////////////////////////////////////////////////////
+
+const projectionMin = (vertices, mat) => {
+    let min = Infinity;
+    
+    for(const vertex of vertices) {
+        multMat3(SAT2d.bufferA, vertex, mat, 1);
+        const val = dot(SAT2d.bufferA, SAT2d.bufferN);
+
+        if(val < min) min = val;
+    }
+
+    return min;
+}
+
+const projectionMax = (vertices, mat) => {
+    let max = -Infinity;
+
+    for(const vertex of vertices) {
+        multMat3(SAT2d.bufferA, vertex, mat, 1);
+        const val = dot(SAT2d.bufferA, SAT2d.bufferN);
+
+        if(val > max) max = val;
+    }
+
+    return max;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
 export class SAT2d {
+
+    static createResultBuffer() {
+        return {
+            axe: new Float32Array([0, 0]),
+            min: 0
+        };
+    }
+
+    /**
+     * Retourne l'axe et la distance minimum de séparation par rapport à colliderA
+     */
     static getMin(colliderA, matA, colliderB, matB, buffer = {axe: new Float32Array(2)}) {
-        buffer.min = 1000;
+        buffer.axe[0] = 0;
+        buffer.axe[1] = 0;
+        buffer.min = 0;
 
-        //////////////////////////////////////////////////////////
-        /*for(const axe of colliderA.axes) {
+        let diff = Infinity;
+        let isColliderA = true;
+        for(const axe of colliderA.axes) {
             multMat3(SAT2d.bufferN, axe.direction, matA, 0);
-            multMat3(SAT2d.bufferA, axe.origin, matA, 1);
+            const max = projectionMax(colliderA.vertices, matA);
+            const min = projectionMin(colliderB.vertices, matB);
 
-            const ref = dot(SAT2d.bufferA, SAT2d.bufferN);
+            const o = max - min;
 
-            let localMin = 0;
-            for(const vertex of colliderB.vertices) {
-                multMat3(SAT2d.bufferB, vertex, matB, 1);
-                const val = dot(SAT2d.bufferB, SAT2d.bufferN);
-                const pos = val - ref;
-
-                if(pos >= 0) continue;
-
-                if(pos > buffer.min) {
-                    localMin = pos;
-                    buffer.min = pos;
-                    buffer.axe[0] = SAT2d.bufferN[0];
-                    buffer.axe[1] = SAT2d.bufferN[1];
-                }
+            if(o <= 0) return buffer;
+            if(diff > o) {
+                diff = o;
+                SAT2d.bufferB[0] = SAT2d.bufferN[0];
+                SAT2d.bufferB[1] = SAT2d.bufferN[1];
             }
-
-            if(localMin >= 0) return null;
-        }*/
-        //////////////////////////////////////////////////////////
+        }
 
         for(const axe of colliderB.axes) {
             multMat3(SAT2d.bufferN, axe.direction, matB, 0);
-            multMat3(SAT2d.bufferA, axe.origin, matB, 1);
+            const min = projectionMin(colliderA.vertices, matA);
+            const max = projectionMax(colliderB.vertices, matB);
 
-            const ref = dot(SAT2d.bufferA, SAT2d.bufferN);
+            const o = max - min;
 
-            let localMin = 0;
-            for(const vertex of colliderA.vertices) {
-                multMat3(SAT2d.bufferB, vertex, matA, 1);
-                const val = dot(SAT2d.bufferB, SAT2d.bufferN);
-                const pos = val - ref;
-
-                //if(pos >= 0) continue;
-
-                if(Math.abs(pos) < buffer.min) {
-                //    localMin = pos;
-                    buffer.min = pos;
-                    buffer.axe[0] = SAT2d.bufferN[0];
-                    buffer.axe[1] = SAT2d.bufferN[1];
-                }
-
-                break;
+            if(o <= 0) return buffer;
+            if(diff > o) {
+                diff = o;
+                isColliderA = false;
+                SAT2d.bufferB[0] = SAT2d.bufferN[0];
+                SAT2d.bufferB[1] = SAT2d.bufferN[1];
             }
-
-            //if(localMin >= 0) return null;
         }
+
+        if(isColliderA) buffer.min = -diff;
+        else buffer.min = diff;
+        
+        buffer.axe[0] = SAT2d.bufferB[0];
+        buffer.axe[1] = SAT2d.bufferB[1];
 
         return buffer;
     }
