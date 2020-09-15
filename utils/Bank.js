aafde39a7311f3e1b249bfae193b51abf659022e
CookieKastanie <jerem0205@yahoo.fr>
2020-09-15 19:08:30 +0200

    ...

--- utils/Bank.js
+++ utils/Bank.js
@@ -1,119 +1,106 @@
-export class Bank {
-  constructor(repertoire, listeNomFichiers, options){
-    this.setDirectory(repertoire);
-    this.setFileList(listeNomFichiers);
-    this.setOptions(options);
-  }
-
-  setDirectory(rep = "./"){
-    this.rep = rep;
-    return this;
-  }
-
-  setFileList(listeNomFichiers = []){
-    this.noms = listeNomFichiers.map(str => str.trim());
-    return this;
-  }
-
-  setOptions(options = {}){
-    this.extension = options.extension ? `.${options.extension}` : "";
-    this.mediaType = options.mediaType || "text";
-    this.treatment = options.treatment || null;
-    this.default = options.default || undefined;
-    return this;
-  }
-
-  initForLoad(){
-    if(this.mediaType === "image") this.mediaType = "img";
-
-    if(this.mediaType === "img" ||
-       this.mediaType === "video" ||
-       this.mediaType === "audio")
-       this.requeteMode = "media";
-    //else if(this.mediaType === "font") this.requeteMode = "font";
-    else this.requeteMode = "file";
-
-    if(this.mediaType !== "arrayBuffer" &&
-       this.mediaType !== "blob" &&
-       this.mediaType !== "formData" &&
-       this.mediaType !== "json" &&
-       //this.mediaType !== "font" &&
-       this.mediaType === "text"
-     ) this.mediaType = "text";
-
-    this.fichiers = new Object();
-  }
-
-  async loadMedia(nom){
+const loadMedia =  async (bank, name) => {
     return new Promise((resolve, reject) => {
-      const elem = document.createElement(this.mediaType);
-      if(this.mediaType == "img") elem.onload = () => {resolve(elem);};
-      else elem.onloadedmetadata = () => {resolve(elem);};
-      elem.onerror = () => { reject(); }
-      elem.src = `${this.rep}/${nom}${this.extension}`;
+        const elem = document.createElement(bank.mediaType);
+        if(bank.mediaType == Bank.IMAGE) elem.onload = () => {resolve(elem);};
+        else elem.onloadedmetadata = () => {resolve(elem);};
+        elem.onerror = () => { reject(); }
+        elem.src = `${bank.dir}/${name}${bank.extension}`;
     });
-  }
+}
 
-  /*async loadFont(nom){
-    const font = new FontFace(nom, `${this.rep}/${nom}${this.extension}`);
-    return font.load()
-    .then(() => {
-      return font;
-    });
-  }*/
-
-  async loadFile(nom){
-    return fetch(`${this.rep}/${nom}${this.extension}`)
+const loadFile = async (bank, name) => {
+    return fetch(`${bank.dir}/${name}${bank.extension}`)
     .then(data => {
-      if(data.ok) return data[this.mediaType]();
-      else return Promise.reject();
-    })
-  }
+        if(data.ok) return data[bank.mediaType]();
+        else return Promise.reject();
+    });
+}
 
-  async load(stateEvent, endMessage = ""){
-    this.initForLoad();
-    if(typeof stateEvent !== "function") stateEvent = (p) => {};
+export class Bank {
+    constructor(directory, fileNames, options = {extension: null, mediaType: null, treatment: null, default: null}){
+        this.setDirectory(directory);
+        this.setFileList(fileNames);
+        this.setOptions(options);
+    }
 
-    return new Promise(async (resolve, reject) => {
-      if(this.noms.length == 0) {
-        stateEvent(100, endMessage);
-        resolve();
-      } else {
-        let nbTotalFichier = this.noms.length;
+    setDirectory(dir = './'){
+        this.dir = dir;
+        return this;
+    }
 
-        let requete;
-        if(this.requeteMode === "media") requete = this.loadMedia.bind(this);
-        //else if(this.requeteMode === "font") requete = this.loadFont.bind(this);
-        else requete = this.loadFile.bind(this);
+    setFileList(fileNames = []){
+        this.names = fileNames.map(str => str.trim());
+        return this;
+    }
 
-        for (const nom of this.noms) {
-          stateEvent(Math.floor((this.noms.length - nbTotalFichier) / this.noms.length * 100), nom);
+    setOptions(options = {extension: null, mediaType: null, treatment: null, default: null}){
+        this.extension = options.extension ? `.${options.extension}` : "";
+        this.mediaType = options.mediaType || Bank.TEXT;
+        this.treatment = options.treatment || null;
+        this.default = options.default || undefined;
+        return this;
+    }
 
-          await requete(nom)
-          .then(async file => {
-            if(typeof this.treatment === "function") this.fichiers[nom] = await this.treatment(file, nom);
-            else this.fichiers[nom] = file;
-            --nbTotalFichier;
-          })
-          .catch(e => {
-            console.warn(`Impossible de charger le fichier '${this.rep}/${nom}${this.extension}'\n${e}`);
-          });
+    initForLoad(){
+        if(this.mediaType === 'image') this.mediaType = Bank.IMAGE;
+
+        if(this.mediaType === Bank.IMAGE || this.mediaType === Bank.VIDEO || this.mediaType === Bank.AUDIO) this.requeteMode = 'media';
+        else this.requeteMode = 'file';
+
+        this.files = new Map();
+        if(typeof this.treatment !== 'function') this.treatment = null;
+    }
+
+    async load(stateEvent, startMessage = '') {
+      this.initForLoad();
+      if(typeof stateEvent !== 'function') stateEvent = () => {};
+
+      return new Promise((resolve, reject) => {
+        if(this.names.length == 0) {
+            resolve();
+            return;
         }
 
-        stateEvent(100, endMessage);
+        const loadFunc = this.requeteMode === 'media' ? loadMedia : loadFile;
 
-        resolve();
-      }
-    })
-  }
+        this.count = 0;
 
-  get(nom){
-    const f = this.fichiers[nom];
-    if (!f) return this.fichiers[this.default];
-    return f;
-  }
+        stateEvent(0, startMessage);
+        
+        for(const name of this.names) {
+            loadFunc(this, name).then(async file => {
+                if(this.treatment) this.files.set(name, await this.treatment(file, name));
+                else this.files.set(name, file);
 
-  getAll(){
-    return this.fichiers;
-  }
+                ++this.count;
+
+                stateEvent(Math.floor((this.count / this.names.length) * 100), name);
+                
+                if(this.count == this.names.length) resolve();
+            }).catch(e => {
+                console.warn(`Impossible de charger le fichier '${this.dir}/${name}${this.extension}'\n${e}`);
+                reject();
+            });
+        }
+      });
+    }
+
+    get(name){
+        const f = this.files.get(name);
+        if(!f) return this.files.get(this.default);
+        return f;
+    }
+
+    getAll(){
+        return this.files;
+    }
 }
+
+Bank.IMAGE = 'img';
+Bank.VIDEO = 'video';
+Bank.AUDIO = 'audio';
+Bank.ARRAY_BUFFER = 'arrayBuffer';
+Bank.BLOB = 'blob';
+Bank.FORM_DATA = 'formData';
+Bank.JSON = 'json';
+Bank.TEXT = 'text';
